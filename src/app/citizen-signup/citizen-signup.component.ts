ae9daf4199f5ced7b142bb0808fb2214a467b3df
Akrem Hammami <akremhammami@outlook.com>
2022-02-12 17:46:21 +0100

    update signup citizen

--- src/app/citizen-signup/citizen-signup.component.ts
+++ src/app/citizen-signup/citizen-signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { GlobalConstants } from '../Shared/user-type';
 
 @Component({
   selector: 'app-citizen-signup',
@@ -7,10 +9,30 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class CitizenSignupComponent implements OnInit {
 
-  constructor() { }
   @Input()
   x!: string;
+ 
+  myForm!: FormGroup;
+  type:string="";
+  constructor(private fb: FormBuilder) { }
+
   ngOnInit(): void {
+    this.myForm = this.fb.group({
+      email: ['', [Validators.required,Validators.email]],
+      password: ['', [Validators.required ]],
+      passwordc: ['', [Validators.required ]],
+      type: ['', [Validators.required ]]
+  
+    });
   }
 
+  onSubmit(form: FormGroup) {
+    console.log('Valid?', form.valid); // true or false
+    console.log('password', form.value.password);
+    console.log('Email', form.value.email);
+    console.log('Email', form.value.type);
+    this.type=form.value.type;
+    console.log(  GlobalConstants.usertype) 
+
+  }
 }
