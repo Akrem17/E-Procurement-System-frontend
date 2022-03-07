2f1bbad58015e7bb0030e0d08d60951834907430
Akrem Hammami <akremhammami@outlook.com>
2022-03-07 17:32:24 +0100

    add signup supplier navigations

--- src/app/signup/supplier-signup/supplier-signup.component.ts
+++ src/app/signup/supplier-signup/supplier-signup.component.ts
@@ -1,10 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { CITIZEN } from 'src/app/Shared/Models/CITIZEN';
-import { SUPPLIER } from 'src/app/Shared/Models/SUPPLIER';
 import { AuthService } from 'src/app/Shared/Services/auth.service';
-import { GlobalConstants } from 'src/app/Shared/user-type';
+import { ValidatorService } from 'src/app/Shared/Services/ValidatorService/validator.service';
 
 @Component({
   selector: 'app-supplier-signup',
@@ -19,38 +17,49 @@ export class SupplierSignupComponent implements OnInit {
   myForm!: FormGroup;
   formCitizen!: FormGroup;
   type:string="";
+  next:boolean=false;
 
-  constructor(private fb: FormBuilder, private _auth :AuthService,private _router:Router) { }
+  constructor(private fb: FormBuilder, private _auth :AuthService,private _router:Router, private _validatorService: ValidatorService) { 
+
+   
+  }
 
   ngOnInit(): void {
     this.myForm = this.fb.group({
       supplierName: ['', [Validators.required]],
       category: ['', [Validators.required ]],
-      registrationNumber: ['', [Validators.required]],
+      registrationNumber: ['', [Validators.required,this._validatorService.ValidateLength]],
       registrationDate: ['', [Validators.required]],
-      taxId: ['', [Validators.required]],
-      cnssId: ['', [Validators.required]],
+      taxId: ['', [Validators.required,Validators.maxLength(7),Validators.minLength(7)]],
+      cnssId: ['', [Validators.required,Validators.maxLength(10),Validators.minLength(10),Validators.required]],
       companyName: ['', [Validators.required]],
       buisnessClassification: ['', [Validators.required]],
-      replyEmail: ['', [Validators.required]],
+      replyEmail: ['', [Validators.required, Validators.email]],
       buisnessType: ['', [Validators.required]],
-      phone: ['', [Validators.required]],
-      fax: ['', [Validators.required]]
-   
-    
-
-     
+      phone: ['', [Validators.required, Validators.min(20000000), Validators.max(99999999)]],
+      fax: ['', [Validators.required, Validators.min(30000000), Validators.max(99999999)]]
 
     });
     
   }
-
+  // get errorMessage(): string {
+  //   const form: FormControl = (this.myForm.get('registrationNumber') as FormControl);
+  //   return form.hasError('required') ?
+  //     'registrationNumber is required' :
+  //     form.hasError('ValidateLength') ?
+  //       'registration Number length should be more than 8 caracters' :
+  //       form.hasError('ValidatePassword') ?
+  //         'Passowrdr' : '';
+  // }
   
   onSubmit(form: FormGroup) {
     console.log(form.value);
     this.basicInfo=form;
     console.log(this.user)
     this.basicInfo.addControl('user', new FormGroup(this.user.controls)); 
+    this.next=true;
+    document.body.scrollTop = document.documentElement.scrollTop = 0;
+    console.log(this.next)
 
     //check buisness registration number
     //check taxid
